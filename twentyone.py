d24f82068600dfa0791e66b477657558103eacda
John <johnr21@users.noreply.github.com>
2017-08-28 12:32:36 -0400

    Revision 1
    Added input validation and conditional. Error when using large values.

--- twentyone.py
+++ twentyone.py
@@ -1,12 +1,34 @@
-x = input("Enter a number: ")
+# Asks the user for a float input
+# Validates input and breaks loop if input is a float
 
-if float(x) == 21:
-    print("test")
-elif float(x) > 21:
-    print("test 2")
-elif float(x) < 21:
-    print("test 3")
-elif type(x) is str:
-    print("test 4")
-else:
-    print("test 5")
+
+
+while True:
+    try:
+        userInput = float(input("Enter a number: "))
+        break
+    except ValueError:
+        print("You lose")
+        continue
+
+# Calculates whatever the user inputs to the number 21
+
+if userInput == 21:
+    print("You win")
+elif userInput > 21:
+    twentyOne = float(21)
+    answer = float(0)
+    difference = userInput - twentyOne
+    result = userInput - difference
+    print(int(result))
+    print("You win")
+elif userInput < 21:
+    twentyOne = float(21)
+    answer = float(0)
+    difference = twentyOne - userInput
+    result = userInput + difference
+    print(int(result))
+    print("You win")
+
+# When the number is to big or to small than the answer is 0
+    
